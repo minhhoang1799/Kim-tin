4ca992f5a5e8d13e773098b12c493a13bbe575a2
minhhoang1799 <hoangvm1799@gmail.com>
2021-12-14 17:20:38 +0700

    up

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
-    
+  slide();
 });
 
+function slide() {
+  $(".homne__banner--slide").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    dots: true,
+    infinite: true,
+  });
+}
